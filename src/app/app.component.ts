dcd39448734bc6dc7d692dc70ec43d62d38169e4
Robin Desarcy <robindesarcy@MacBook-Pro-de-Robin.local>
2020-12-22 16:25:56 +0100

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import firebase from '@firebase/app';
+import '@firebase/auth';
+import '@firebase/database';
+//import '@firebase/firestore';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +10,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'bookshelves';
+  constructor() {
+    var firebaseConfig = {
+      apiKey: "AIzaSyBmDFnfbTketOMkDhX5Hz2e7Hk9Y9cRO-o",
+      authDomain: "book-shelves-c80ba.firebaseapp.com",
+      projectId: "book-shelves-c80ba",
+      storageBucket: "book-shelves-c80ba.appspot.com",
+      messagingSenderId: "675511625653",
+      appId: "1:675511625653:web:3e6808633a213088dc5bfc"
+    };
+    firebase.initializeApp(firebaseConfig);
+  }
 }
